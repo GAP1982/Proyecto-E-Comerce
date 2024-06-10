cf47035615dcc2bc05333ee2d71b61627d29ccef
someone <someone@someplace.com>
2024-06-10 19:06:28 -0300

    se modificaron archivos

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
 const app = express()
-app.listen (3000, ()=> {
-    console.log('servidor escuchando en el puerto 3000')
+app.listen (8000, ()=> {
+    console.log('servidor escuchando en el puerto 8000')
 })
